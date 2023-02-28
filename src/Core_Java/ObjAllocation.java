9c67f732ad11deb5a5593602580967e6640d7685
Uday Tiwari <udaytiwari360@outlook.com>
2023-02-28 17:05:44 +0530

    Modified 'Objalloccation.java'

--- src/Core_Java/ObjAllocation.java
+++ src/Core_Java/ObjAllocation.java
@@ -25,10 +25,19 @@ public class ObjAllocation {
  
         Demo D = new Demo();
         D.setter(3);
-        System.out.println(D.setMe);
+        System.out.println(D.setMe); // 3
         D.greet();
 
+        Demo B = D;
+
+        System.out.println(B.setMe); // 3 : Referencing to the same object as of 'D'!
+
         D = new Demo(); // Reinitialized with a different address in heap
-        System.out.println(D.setMe);
+        System.out.println(D.setMe); // 0
+        
+        D.setter(10);
+        System.out.println(D.setMe); // 10
+        System.out.println(B.setMe); // 3 : Since it is referencing to the same old object!
+
     }
 }
